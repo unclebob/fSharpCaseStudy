c7a2e019cbffd91f4ad45dcad30142e225a5d139
Robert C. Martin <unclebob@cleancoder.com>
2017-12-14 11:32:27 -0600

    E3T2

--- getargs/getargsTest.fs
+++ getargs/getargsTest.fs
@@ -8,8 +8,12 @@ open FsUnit
 
 //myCommand -s Bob -d 1.3 -n 42
 
+let makeTokens schema = 
+     schema
+
 let getArgs schema args = 
-    []
+    [],args
+    
 
 
 [<Test>]
@@ -17,16 +21,36 @@ let ``no arguments and no schema``() =
      let args = []
      let schema = ""
      let foundArgs, unfoundArgs = args |> getArgs schema
-     config |> foundArgs |> should haveLength 0
+     foundArgs |> should haveLength 0
+     unfoundArgs |> should haveLength 0
      
 [<Test>]
 let ``no schema but with args``() =
      let args = ["-n"; "1"; "-d"; "3.14"]
      let schema = ""
-     let config = args |> getArgs schema
-     let foundArgs, unfoundArgs = config |> 
-     config |> foundArgs |> should haveLength 0 
-     config |> unfoundArgs |> should haveLength 4   
+     let foundArgs, unfoundArgs = args |> getArgs schema
+     foundArgs |> should haveLength 0 
+     unfoundArgs |> should haveLength 4   
+     
+[<Test>]
+let ``break simple schema into tokens``() =    
+     let schema = "b"
+     schema |> makeTokens |> should equal ["b"]
+     
+
+     
+[<Test>]
+[<Ignore("integration test for later")>]
+let ``single int schema with appropriate arg``() =
+     let args = ["-b"]
+     let schema = "b" 
+     let foundArgs, unfoundArgs = args |> getArgs schema
+     foundArgs |> should haveLength 1 
+     unfoundArgs |> should haveLength 0    
+     
+   
+     
+
      
 
      
